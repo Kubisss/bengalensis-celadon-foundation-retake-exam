9191915acff48ba0d6f1476ac0de8621baacc531
Jakub Frýdek <jakub.frydek@seznam.cz>
2022-01-14 09:13:02 +0100

    started with matrix and worstSongs

--- src/multiplyItemsInMatrix/MultiplyItemsInMatrix.java
+++ src/multiplyItemsInMatrix/MultiplyItemsInMatrix.java
@@ -1,4 +1,10 @@
 package multiplyItemsInMatrix;
 
 public class MultiplyItemsInMatrix {
+
+    public static int [][] multiplyItemsInMatrix (int [][] matrix) {
+
+
+        return matrix;
+    }
 }
--- src/worstSongs/WorstSongs.java
+++ src/worstSongs/WorstSongs.java
@@ -1,4 +1,23 @@
 package worstSongs;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class WorstSongs {
+    public static void main(String[] args) throws IOException {
+        worstYearInMusic("100worst.csv");
+
+    }
+
+    public static int worstYearInMusic(String filename) throws IOException {
+        Path path = Paths.get("src/worstSongs/"+filename);
+        List<String> worstSongs = Files.readAllLines(path);
+
+        System.out.println(worstSongs);
+
+        return 0;
+    }
 }
